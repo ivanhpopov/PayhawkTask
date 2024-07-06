b3e069c2251d19c495cf7e4829077e9b2ee72dc2
ipopov <140158056+ivanhpopov@users.noreply.github.com>
2024-07-06 17:01:49 +0300

    Api file complete for a single instance, need to get this looped for when the files are multiple. Will proceed to finalize the GUI and put it together.

--- API.py
+++ API.py
@@ -1,12 +1,16 @@
 import requests
+import json
 
 def check_ip_on_virustotal(ip_address, api_key):
     url = f'https://www.virustotal.com/api/v3/ip_addresses/{ip_address}'
     headers = {"X-Apikey": api_key}
     response = requests.get(url, headers=headers)
-    return response
+    content = json.loads(response.content)
+    ip_dict = content
+    repuation = ip_dict["data"]["attributes"]["reputation"]
+    print(f"Reputation of {ip_address} is: {repuation} (higher is better)")
     
-ip = '133.12.32.43'
-API_key = input("Please insert your API Key to proceed")
+ip_address = input("Give me an IP:")
+api_key = '656eb69d98741d422816b890c809f1d8ac78ff4ac5e225a0048ad11cfdb9bd06'
 
-check_ip_on_virustotal(ip, API_key )
+check_ip_on_virustotal(ip_address, api_key)
